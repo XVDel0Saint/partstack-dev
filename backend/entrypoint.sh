9c006367751ea3598754254fe93ae22b1c2d9842
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2026-02-11 02:42:18 +0800

    updates on entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
-# checks if the users table exists
+echo "Starting container..."
+
+# Run migrations + seeders if DB is empty
 TABLE_CHECK=$(php artisan tinker --execute "echo Schema::hasTable('users') ? '1' : '0';")
 
 if [ "$TABLE_CHECK" = "0" ]; then
-    echo "Running migrations and seeding..."
+    echo "Running migrations..."
     php artisan migrate --force
+    echo "Running seeders..."
     php artisan db:seed --class=DatabaseSeeder
 else
     echo "Migrations already applied, skipping."
 fi
 
-# Start PHP-FPM and Nginx
+# Generate Passport keys if missing
+if [ ! -f storage/oauth/private.key ] || [ ! -f storage/oauth/public.key ]; then
+    echo "Generating Laravel Passport keys..."
+    php artisan passport:install --force
+else
+    echo "Passport keys already exist."
+fi
+
+
+# Starts PHP-FPM + Nginx
+echo "Starting PHP-FPM and Nginx..."
 php-fpm -D
 nginx -g 'daemon off;'
