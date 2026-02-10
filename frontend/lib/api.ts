a2550c1cb50128760ab7147297bbba41900db8ef
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2026-02-11 03:30:46 +0800

    minor optimizations in api library

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Accept": "application/json",
-    //"Content-Type": "application/json",
+    "Content-Type": "application/json",
   },
 })
 
