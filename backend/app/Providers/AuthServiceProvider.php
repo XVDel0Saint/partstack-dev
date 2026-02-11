04d53fd0af9958ae18da07ccd767e7764e4ccc23
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2026-02-11 08:38:28 +0800

    reoptimized file

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -7,30 +7,30 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    // protected $policies = [];
-
-    // public function boot(): void
-    // {
-    //     Passport::loadKeysFrom(__DIR__.'/../../storage');
-    //     Passport::tokensExpireIn(now()->addHours(2));
-    //     Passport::refreshTokensExpireIn(now()->addDays(30)); // Refresh token
-    // }
+    protected $policies = [];
 
     public function boot(): void
     {
-        $this->registerPolicies();
-
-        // Point exactly to the folder where your entrypoint.sh creates the keys
-        Passport::loadKeysFrom(storage_path('oauth'));
-
-        // Force Passport to use the Client ID and Secret from your Environment Variables
-        Passport::usePersonalAccessClient(
-            config('passport.personal_access_client.id'),
-            config('passport.personal_access_client.secret')
-        );
-
-        // Optional: If you use Personal Access Tokens, set their expiry too
-        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+        Passport::loadKeysFrom(__DIR__.'/../../storage');
+        Passport::tokensExpireIn(now()->addHours(2));
+        Passport::refreshTokensExpireIn(now()->addDays(30)); // Refresh token
     }
+
+    // public function boot(): void
+    // {
+    //     $this->registerPolicies();
+
+    //     // Point exactly to the folder where your entrypoint.sh creates the keys
+    //     Passport::loadKeysFrom(storage_path('oauth'));
+
+    //     // Force Passport to use the Client ID and Secret from your Environment Variables
+    //     Passport::usePersonalAccessClient(
+    //         config('passport.personal_access_client.id'),
+    //         config('passport.personal_access_client.secret')
+    //     );
+
+    //     // Optional: If you use Personal Access Tokens, set their expiry too
+    //     Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+    // }
 
 }
